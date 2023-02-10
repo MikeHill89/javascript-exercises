c92804864ebef7b011e9acc2e13b901b546f890e
MikeHill89 <mvreugdenhil18@gmail.com>
2023-02-10 20:35:28 +0200

    Fix: first test passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,9 @@
-const repeatString = function() {
-
+const repeatString = (string, num) => {
+  let finalString = '';
+  for (i=1; i <=num; i++) {
+    finalString += string;
+  }
+  return finalString
 };
-
 // Do not edit below this line
 module.exports = repeatString;

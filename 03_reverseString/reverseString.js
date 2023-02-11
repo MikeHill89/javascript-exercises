ffdaf129703fbd61f786af2240df5cd6280f9670
MikeHill89 <mvreugdenhil18@gmail.com>
2023-02-11 18:21:58 +0200

    Fix: All tests passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = (string) => {
+  // takes the string argument
+  // splits each charachter in a new array without spaces
+  // Reverses items from the new array. 
+  // Joins the reversed array back into a string and separates them by no space, but can include space as an array item
+  return string.split("").reverse().join("");
+  
 };
 
 // Do not edit below this line

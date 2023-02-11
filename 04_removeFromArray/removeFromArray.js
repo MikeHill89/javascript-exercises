1356ecae08fa35ed2a77f5af9c092af232c72697
MikeHill89 <mvreugdenhil18@gmail.com>
2023-02-11 18:44:49 +0200

    Fix: passed 1st test.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, item) {
+  
+  const itemIndex = array.indexOf(item);
+  array.splice(itemIndex,1);
+  return array;
+  
 };
 
 // Do not edit below this line

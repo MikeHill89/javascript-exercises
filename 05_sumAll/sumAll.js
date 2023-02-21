7ee8046f63ad6d34d05fb950c0d28413ec7f07cd
MikeHill89 <mvreugdenhil18@gmail.com>
2023-02-21 23:59:36 +0200

    Fix: all 6 test passing

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -3,7 +3,7 @@ const sumAll = function(a,b) {
     let highNumber = Math.max(a,b);
     
     let sum = 0;
-    if ( a < 0 || b < 0){
+    if ( a < 0 || b < 0 || (typeof a) !== "number" || (typeof b) !== "number" ){
         return "ERROR";
     } else {
         for (let i = lowNumber; i < highNumber +1; i++){

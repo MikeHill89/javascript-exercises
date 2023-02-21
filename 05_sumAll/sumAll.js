9118975baf59af9df81c5e07e007234f32705191
MikeHill89 <mvreugdenhil18@gmail.com>
2023-02-21 23:46:46 +0200

    Fix: 4 test passed 2 remaining

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
-
-};
-
+const sumAll = function(a,b) {
+    let lowNumber = Math.min(a,b);
+    let highNumber = Math.max(a,b);
+    
+    let sum = 0;
+    if ( a < 0 || b < 0){
+        return "ERROR";
+    } else {
+        for (let i = lowNumber; i < highNumber +1; i++){
+            sum +=i;
+        }
+        return sum;
+    }
+}
 // Do not edit below this line
 module.exports = sumAll;

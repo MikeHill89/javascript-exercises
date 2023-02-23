f76de55339439ec082cc0f5107d129f27e0656c4
MikeHill89 <mvreugdenhil18@gmail.com>
2023-02-23 15:00:30 +0200

    Fix: All tests passed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,9 +1,11 @@
 const leapYears = function(year) {
-    if (year % 4 === 0 && (!year / 100 || year / 400)) {
-       return true;
+
+    if ((year % 4 === 0 ) && (year % 100 !== 0) || year % 400 === 0){
+        return true;
     } else {
         return false;
     }
+
 };
 
 // Do not edit below this line

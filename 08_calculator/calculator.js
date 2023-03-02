59feb16584712fbcda487972d8682f5c590a22c8
MikeHill89 <mvreugdenhil18@gmail.com>
2023-03-02 21:18:57 +0200

    Fix: Sum and multiply working with spread operator. Bug: multiple arrays, needs flattening and filtering

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,36 +1,44 @@
 const add = function(...numbers) {
-    let sum = 0;
-    for (let number of numbers) sum += Number(number);
-    return sum;
-	
+    let result = numbers[0];
+    for (let i = 1; i < numbers.length; i++) {
+        result += numbers[i];
+    }
+    return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(...numbers) {
+    let result = numbers[0];
+    for (let i = 1; i < numbers.length; i++) {
+        result -= numbers[i];
+    }
+    return result;
 };
 
-const sum = function() {
-	
-};
+const sum = function(...numbers) {
+    numbers = numbers[0];
+    return numbers.reduce((total, num) => total + num, 0);
+}
 
-const multiply = function() {
-
+const multiply = function(...numbers) {
+    numbers = numbers[0]
+    return numbers.reduce((total, num) => total * num, 1);
+       
 };
 
 const power = function() {
-	
+
 };
 
 const factorial = function() {
-	
+
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial
 };

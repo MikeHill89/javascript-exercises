ff714f8e1414391c9e075d341edf74a6269117c6
MikeHill89 <mvreugdenhil18@gmail.com>
2023-03-08 23:57:00 +0200

    All tests passed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(count) {
+    let initialValue = 0;
+    let newValue = 1;
+    if (count < 0) {
+        return "OOPS"
+    } else {
+        for (let i = 1; i < count; i++) {
+            const tempValue = newValue;
+            newValue = initialValue + newValue;
+            initialValue = tempValue;
+        }
+        return newValue;
+    }
 };
 
 // Do not edit below this line

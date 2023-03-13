8bda0d0a4920097ccc0c1f87d112b50587cea5fb
MikeHill89 <mvreugdenhil18@gmail.com>
2023-03-13 20:31:41 +0200

    Fix: 1st test passed

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,4 +1,14 @@
-const caesar = function() {
+const caesar = function(string) {
+   const letterArray = Array.from(string)
+
+    for (let i = 0; i < letterArray.length; i++) {
+        let shiftFactor = 1;
+        const charCode = letterArray[i].charCodeAt(i);
+        const encodedLetter = charCode + shiftFactor;
+        return String.fromCharCode(encodedLetter);
+        
+
+    }
 
 };
 

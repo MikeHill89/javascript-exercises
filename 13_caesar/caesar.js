03ab75a81ef4a294d7f612d08a5da1d6b19e6adc
MikeHill89 <mvreugdenhil18@gmail.com>
2023-03-19 19:23:09 +0200

    Fix: Works with single letters and words.

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,16 +1,15 @@
-const caesar = function(string) {
-   const letterArray = Array.from(string)
+const caesar = function (string, shiftfactor) {
 
-    for (let i = 0; i < letterArray.length; i++) {
-        let shiftFactor = 1;
-        const charCode = letterArray[i].charCodeAt(i);
-        const encodedLetter = charCode + shiftFactor;
-        return String.fromCharCode(encodedLetter);
+    const splitString = string.split("");
         
-
-    }
-
+    for (let index = 0; index < splitString.length; index++) {
+        const charCode = splitString[index].charCodeAt();
+        if (charCode === 44 || charCode === 33 || charCode === 32) continue;
+        const charCodeShift = charCode + shiftfactor;
+        splitString[index] = String.fromCharCode(charCodeShift)
+    };
+    
+    return splitString.join("");
 };
-
 // Do not edit below this line
 module.exports = caesar;
